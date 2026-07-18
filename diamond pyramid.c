3bc748dc3fb817f8cf47a4947dc49d8f39193645
sanjyotkale26-ai <sanjyotkale26@gmail.com>
2026-07-18 15:18:08 +0530

    Create diamond pyramid.c

---
+++ diamond pyramid.c
@@ -0,0 +1,30 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int i, j, n;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++)
+    {
+        // Print spaces
+        for(j = 1; j <= n - i; j++)
+        {
+            printf(" ");
+        }
+
+        // Print numbers
+        for(j = 1; j <= i; j++)
+        {
+            printf("%d ", i);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
+
